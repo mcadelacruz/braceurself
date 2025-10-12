b51553333c99b84bc48e737aa711a10f8d1fd6b4
mcadelacruz <mcadelacruz@tip.edu.ph>
2025-10-13 00:52:35 +0800

    some hosting changes

--- braceurself/settings.py
+++ braceurself/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a-!(d%qzm-!&j6a3puxni+!1lpe0)ak-(asg&rt3zm64ms5xob
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yourusername.pythonanywhere.com']
 
 
 # Application definition
